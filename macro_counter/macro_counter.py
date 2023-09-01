f427a9e33ba58179ecd29b6e4dc76db88f5d05fd
Vallen217 <EidolonVallen@gmail.com>
2023-08-31 19:59:02 -0500

    Update to match PEP-8

--- macro_counter/macro_counter.py
+++ macro_counter/macro_counter.py
@@ -2,6 +2,11 @@ import os
 import re
 from datetime import datetime
 
+
+def pad(word):
+    return " " * (12 - len(str(word)))
+
+
 class MacroCounter:
     def __init__(self, target_directory, target_file):
         self.target_directory = target_directory
@@ -16,30 +21,28 @@ class MacroCounter:
             os.mkdir(self.target_directory)
         os.chdir(self.target_directory)
 
-        try:
-            with open(self.target_file, 'x') as _:
+        if not os.path.exists(self.target_file):
+            with open(self.target_file, "x", encoding="utf-8"):
                 pass
 
-        except FileExistsError:
-            pass
-
-        if predefined == True:
+        if predefined is True:
             return predefined_meals()
-        else:
-            return
+        return None  # 'return None' statement to satisfy pylint diagnostics
 
     # saves the file data to rewrite it in write_file() with the new data.
     def compile_data(self, file_name, clean_data=False):
-        # self.data must be cleared to properly recompile data in certain cases
+        # self.data must be cleared to properly recompile data in some cases
         if clean_data:
-            [self.data[i].clear() for i in range(len(self.data))]
+            for i in range(len(self.data)):
+                self.data[i].clear()
+            # [self.data[i].clear() for i in range(len(self.data))]
 
-        with open(file_name, 'r') as rf:
-            for line in rf:
+        with open(file_name, "r", encoding="utf-8") as read_file:
+            for line in read_file:
                 if line == "\n":
                     break
-                
-                if re.match("\d+\.\dg?", line):
+
+                if re.match(r"\d+\.\dg?", line):
                     for i, datum in enumerate(line.split()):
                         if "g" in datum[-1]:
                             datum = datum[0:-1]
@@ -54,14 +57,13 @@ class MacroCounter:
                             self.data[2].append(datum)
                         if (i - 1) % 2 == 0 and (i - 1) % 4 != 0:
                             self.data[3].append(datum)
-        return
 
     def modify_file(self):
         print(
-                "\n(rln)  - Removes the last n file entry lines"
-                "\n(rlqn) - Removes the last n file entry lines and quit "
-                "\n(q)    - Quit the loop\n"
-                "\nPress any key to continue"
+            "\n(rl#)  - Removes the last n file entry lines"
+            "\n(rlq#) - Removes the last n file entry lines and quit "
+            "\n(q)    - Quit the loop\n"
+            "\nPress any key to continue"
         )
         while True:
             operation = str(input("-")).lower()
@@ -69,10 +71,13 @@ class MacroCounter:
                 break
 
             if re.match("rlq?[0-9]*", operation):
-                iterations = int(operation[3:]) if "q" in operation\
-                        else int(operation[2:])
+                iterations = (
+                    int(operation[3:]) if "q" in operation else int(operation[2:])
+                )
                 for i in range(iterations):
-                    [self.data[j].pop() for j in range(4)]
+                    for j in range(4):
+                        self.data[j].pop()
+
                 if "q" in operation:
                     break
 
@@ -84,7 +89,7 @@ class MacroCounter:
 
             except ValueError:
                 break
-            
+
         # catch potential index errors.
         for i in range(4):
             if len(self.data[i]) != len(self.data[3]):
@@ -93,42 +98,46 @@ class MacroCounter:
 
     def write_file(self):
         self.totals = [sum(self.data[i]) for i in range(len(self.data))]
-        pad = lambda word: " " * (12 - len(str(word)))
         ratio = 100 / (self.totals[1] + self.totals[2] + self.totals[3])
         temp = [f"{round(ratio * self.totals[i], 1)}%" for i in range(1, 4)]
         relative_per = [f"{v}{pad(v)}" for v in temp]
 
-        with open(self.target_file, 'w') as wf:
-            wf.write(
-                    f"Cal:{pad('Cal:')}Fat:{pad('Fat:')}"
-                    f"Carb:{pad('Carb:')}Protein:{pad('Protein:')}"
+        with open(self.target_file, "w", encoding="utf-8") as write_file:
+            write_file.write(
+                f"Cal:{pad('Cal:')}Fat:{pad('Fat:')}"
+                f"Carb:{pad('Carb:')}Protein:{pad('Protein:')}"
             )
 
-        with open(self.target_file, 'a') as af:
+        with open(self.target_file, "a", encoding="utf-8") as append_file:
             for i in range(len(self.data[0])):
-                af.write("\n")
+                append_file.write("\n")
                 for j in range(4):
                     if j == 0:
-                        af.write(f"{self.data[j][i]}"
-                                f"{pad(self.data[j][i])}")
+                        append_file.write(
+                            f"{self.data[j][i]}" f"{pad(self.data[j][i])}"
+                        )
                     else:
-                        af.write(f"{self.data[j][i]}g"
-                        f"{pad(f'{self.data[j][i]}g')}")
+                        append_file.write(
+                            f"{self.data[j][i]}g" f"{pad(f'{self.data[j][i]}g')}"
+                        )
 
-            af.write(
-                    f"\n\nTotal Amounts & Relative Percentages:\n"
-                    f"{self.totals[0]}{pad(self.totals[0])}"
-                    f"{self.totals[1]}g{pad(f'{self.totals[1]}g')}"
-                    f"{self.totals[2]}g{pad(f'{self.totals[2]}g')}"
-                    f"{self.totals[3]}g"
-                    f"\n{' ' * 12}{relative_per[0]}"
-                    f"{relative_per[1]}{relative_per[2]}"
-                    )
+            append_file.write(
+                f"\n\nTotal Amounts & Relative Percentages:\n"
+                f"{self.totals[0]}{pad(self.totals[0])}"
+                f"{self.totals[1]}g{pad(f'{self.totals[1]}g')}"
+                f"{self.totals[2]}g{pad(f'{self.totals[2]}g')}"
+                f"{self.totals[3]}g"
+                f"\n{' ' * 12}{relative_per[0]}"
+                f"{relative_per[1]}{relative_per[2]}"
+            )
         return display_data(self.target_file)
+
 
 # This is an appalling mess, but it works.
 def predefined_meals():
-    target_directory= '/home/vallen/Workspace/MacroCounter/MacroCounter/Predefined_Meals'
+    target_directory = (
+        "/home/vallen/Workspace/macro_counter/macro_counter/predefined_meals"
+    )
     print(
         "\n(cf)  - Create new predefined meal"
         "\n(mf)  - Modify predefined meal"
@@ -148,7 +157,8 @@ def predefined_meals():
 
         if operation == "mf":
             print("Enter a file to modify from:")
-            [print(file) for file in os.listdir(target_directory)]
+            for file in os.listdir(target_directory):
+                print(file)
 
             try:
                 file_name = str(input())
@@ -159,65 +169,72 @@ def predefined_meals():
                     raise FileNotFoundError
 
             except FileNotFoundError:
-                print(f"Error: '{file_name}' is not a valid file")
-                return predefined_meals() 
+                print("Error: invalid file name.")
+                return predefined_meals()
 
             target_file = os.path.join(target_directory, file_name)
             counter = MacroCounter(target_directory, target_file)
             counter.compile_data(target_file, clean_data=True)
             counter.modify_file()
-        
+
         if operation == "df":
             view_previous_data(target_directory, operation, predefined=True)
 
+
 def display_data(file):
     print()
-    with open(file, 'r') as rf:
-        for data in rf:
+    with open(file, "r", encoding="utf-8") as read_file:
+        for data in read_file:
             print(data.rstrip("\n"))
     print("\n")
 
+
 def display_monthly_data(directory):
-    pad = lambda word: " " * (12 - len(str(word)))
     totals = [0, 0, 0, 0]
     os.chdir(directory)
     file_list = os.listdir(directory)
 
     for file in file_list:
-        with open(f"{directory}/{file}") as rf:
-            lines = rf.readlines()
-        temp = [lines[i+2].split() for\
-                i, line in enumerate(lines) if line == "\n"]
+        with open(f"{directory}/{file}", encoding="utf-8") as read_file:
+            lines = read_file.readlines()
+        temp = [lines[i + 2].split() for i, line in enumerate(lines) if line == "\n"]
         for j in range(4):
             if "g" in temp[0][j]:
                 temp[0][j] = temp[0][j][0:-1]
+            # Ignore the GeneralTypeError below. This operation works fine.
             totals[j] += float(temp[0][j])
 
-    rel_monthly_per = [f"{round((100 / (sum(totals[1:]))) *  totals[i], 1)}%"\
-            for i in range(1, 4)]
+    relative_monthly_percent = [
+        f"{round((100 / (sum(totals[1:]))) *  totals[i], 1)}%" for i in range(1, 4)
+    ]
     mean_amounts = [round(totals[i] / len(file_list), 1) for i in range(4)]
 
     print(
-            f"\nCal:{pad('Cal:')}Fat:{pad('Fat:')}"
-            f"Carb:{pad('Carb:')}Protein:{pad('Protein:')}\n"
+        f"\nCal:{pad('Cal:')}Fat:{pad('Fat:')}"
+        f"Carb:{pad('Carb:')}Protein:{pad('Protein:')}\n"
     )
-    print(f"Contemporary monthly total amounts: ")
+    print("Contemporary monthly total amounts: ")
     print(f"{totals[0]}{pad(totals[0])}", end="")
-    [print(f"{v}g{pad(f'{v}g')}", end='') for v in totals[1:]]
+    for val in totals[1:]:
+        print(f"{val}g{pad(f'{val}g')}", end="")
 
     print("\n\nMean daily amounts: ")
     print(f"{mean_amounts[0]}{pad(mean_amounts[0])}", end="")
-    [print(f"{v}g{pad(f'{v}g')}", end='') for v in mean_amounts[1:]]
+    for val in mean_amounts[1:]:
+        print(f"{val}g{pad(f'{val}g')}", end="")
 
     print("\n\nMean daily relative percentages: ")
     print(pad(""), end="")
-    [print(f"{v}{pad(v)}", end="") for v in rel_monthly_per]
+    for val in relative_monthly_percent:
+        print(f"{val}{pad(val)}", end="")
     print("\n")
+
 
 def view_previous_data(parent_directory, operation, predefined=False):
     if not predefined:
         print("\nEnter a relative directory from:")
-        [print(dir) for dir in os.listdir(parent_directory)]
+        for directory in os.listdir(parent_directory):
+            print(directory)
 
         try:
             directory_name = str(input("\n"))
@@ -225,35 +242,36 @@ def view_previous_data(parent_directory, operation, predefined=False):
                 raise FileNotFoundError
 
         except FileNotFoundError:
-            print(f"Error: '{directory_name}' is not a valid directory.")
-            return
+            print("Error: invalid directory.")
+            return None  # 'return None' statement to satisfy pylint diagnostics
 
         target_directory = os.path.join(parent_directory, directory_name)
 
     else:
-        target_directory= parent_directory
+        target_directory = parent_directory
 
     if operation == "dpm":
         return display_monthly_data(target_directory)
 
-    else:
-        print("\nEnter a file to view from:")
-        [print(file) for file in os.listdir(target_directory)]
+    print("\nEnter a file to view from:")
+    for file in os.listdir(target_directory):
+        print(file)
 
-        try:
-            file_name = str(input("\n"))
-            if ".txt" not in file_name:
-                file_name = f"{file_name}.txt"
+    try:
+        file_name = str(input("\n"))
+        if ".txt" not in file_name:
+            file_name = f"{file_name}.txt"
 
-            if file_name not in os.listdir(target_directory):
-                raise FileNotFoundError
+        if file_name not in os.listdir(target_directory):
+            raise FileNotFoundError
 
-        except FileNotFoundError:
-            print(f"Error: '{file_name}' is not a valid file.")
-            return
+    except FileNotFoundError:
+        print("Error: invalid file.")
+        return None  # 'return None' statement to satisfy pylint diagnostics
 
-        target_file = os.path.join(target_directory, file_name)
-        return display_data(target_file)
+    target_file = os.path.join(target_directory, file_name)
+    return display_data(target_file)
+
 
 def main():
     print(
@@ -265,17 +283,17 @@ def main():
         "\n(pd)  - Predefined meals"
         "\n(m#)  - Append predefined meal m#"
         "\n(q)   - Quit the program"
-        )
+    )
 
-    parent_directory = '/home/vallen/Documents/Health/Macronutritional_intake'
-    directory_name = f'{datetime.now().year}-{datetime.now().month}'
+    parent_directory = "/home/vallen/Documents/Health/Macronutritional_intake"
+    directory_name = f"{datetime.now().year}-{datetime.now().month}"
     target_directory = os.path.join(parent_directory, directory_name)
-    file_name = f'{datetime.now().day}.txt'
+    file_name = f"{datetime.now().day}.txt"
     target_file = os.path.join(target_directory, file_name)
 
     counter = MacroCounter(target_directory, target_file)
     counter.check_existence()
-    
+
     while True:
         operation = str(input("-")).lower()
         if operation == "q":
@@ -284,12 +302,14 @@ def main():
         if operation == "pd":
             return predefined_meals()
 
-        if operation == "dpf" or operation == "dpm":
+        if operation in ("dpf", "dpm"):
             view_previous_data(parent_directory, operation)
 
         if re.match("m[0-9]+", operation):
-            predefined_file = '/home/vallen/Workspace/MacroCounter'\
-                    f'/MacroCounter/Predefined_Meals/{operation}.txt'
+            predefined_file = (
+                "/home/vallen/Workspace/macro_counter"
+                f"/macro_counter/predefined_meals/{operation}.txt"
+            )
             # 1st function call is to save pre-existing file data.
             # 2nd function call is to append predefined data to the file.
             counter.compile_data(target_file, clean_data=True)
@@ -303,7 +323,11 @@ def main():
             display_data(target_file)
         if operation == "dm":
             display_monthly_data(target_directory)
-    return
+    return None  # 'return None' statement to satisfy pylint diagnostics
+
 
 if __name__ == "__main__":
     main()
+
+# TODO: create docstrings
+# TODO: your mother
